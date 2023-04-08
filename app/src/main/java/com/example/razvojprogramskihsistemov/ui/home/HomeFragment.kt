c4d45cc2f9ec29b1bc216b947b5d3c3a6ed7bcf5
viktordenac <viktor.denac@gmail.com>
2023-04-08 16:49:34 +0200

    Osnovni urnik za študenta

--- app/src/main/java/com/example/razvojprogramskihsistemov/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/razvojprogramskihsistemov/ui/home/HomeFragment.kt
@@ -4,39 +4,56 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
-import com.example.razvojprogramskihsistemov.databinding.FragmentHomeBinding
+import com.example.razvojprogramskihsistemov.R
 
 class HomeFragment : Fragment() {
 
-    private var _binding: FragmentHomeBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
+    private lateinit var scheduleInput: EditText
+    private lateinit var mondayTextView: TextView
+    private lateinit var tuesdayTextView: TextView
+    private lateinit var wednesdayTextView: TextView
+    private lateinit var thursdayTextView: TextView
+    private lateinit var fridayTextView: TextView
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
 
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        scheduleInput = view.findViewById(R.id.schedule_input)
+        mondayTextView = view.findViewById(R.id.monday_textview)
+        tuesdayTextView = view.findViewById(R.id.tuesday_textview)
+        wednesdayTextView = view.findViewById(R.id.wednesday_textview)
+        thursdayTextView = view.findViewById(R.id.thursday_textview)
+        fridayTextView = view.findViewById(R.id.friday_textview)
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
+        // Set click listeners for each day of the week
+        mondayTextView.setOnClickListener { updateScheduleText(mondayTextView.text.toString()) }
+        tuesdayTextView.setOnClickListener { updateScheduleText(tuesdayTextView.text.toString()) }
+        wednesdayTextView.setOnClickListener { updateScheduleText(wednesdayTextView.text.toString()) }
+        thursdayTextView.setOnClickListener { updateScheduleText(thursdayTextView.text.toString()) }
+        fridayTextView.setOnClickListener { updateScheduleText(fridayTextView.text.toString()) }
+
+        return view
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // set the schedule text for each day
+        mondayTextView.text = "Monday: 9:00 AM - 5:00 PM"
+        tuesdayTextView.text = "Tuesday: 9:00 AM - 5:00 PM"
+        wednesdayTextView.text = "Wednesday: 9:00 AM - 5:00 PM"
+        thursdayTextView.text = "Thursday: 9:00 AM - 5:00 PM"
+        fridayTextView.text = "Friday: 9:00 AM - 5:00 PM"
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    private fun updateScheduleText(text: String) {
+        scheduleInput.setText(text)
     }
 }
+
