e9a99dbdbf232de46788b1ff4bc48bf0031d7e71
viktordenac <viktor.denac@gmail.com>
2023-04-10 00:58:50 +0200

    Saving schedule in json

--- app/src/main/java/com/example/razvojprogramskihsistemov/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/razvojprogramskihsistemov/ui/home/HomeFragment.kt
@@ -14,6 +14,10 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.razvojprogramskihsistemov.R
+import org.json.JSONArray
+import org.json.JSONException
+import org.json.JSONObject
+import java.io.*
 
 class HomeFragment : Fragment() {
 
@@ -56,6 +60,7 @@ class HomeFragment : Fragment() {
                     val scheduleText = scheduleInput.text.toString()
                     selectedDay.text = "${selectedDay.text.toString().substringBefore("\n")}\n$scheduleText"
                     Toast.makeText(requireContext(), "Schedule updated!", Toast.LENGTH_SHORT).show()
+                    saveScheduleToFile()
                 }
             }
              else {
@@ -138,19 +143,184 @@ class HomeFragment : Fragment() {
         selectedDayTextView?.setBackgroundColor(resources.getColor(R.color.black))
     }
 
+    private fun saveScheduleToFile() {
+        val scheduleArray = JSONArray()
+        scheduleArray.put(JSONObject().apply {
+            put("day", "Monday")
+            val string: String = mondayTextView.text.toString().substringAfter("\n")
+            put("time", string.substringBefore("\n").trim())
+            var note: String = string.substringAfter("PM")
+            if( note != ""){
+                note = note.substringAfter("Note:")
+            }else{
+                note = ""
+            }
+            put("notes",note.trim())
+        })
+        scheduleArray.put(JSONObject().apply {
+            put("day", "Tuesday")
+            val string: String = tuesdayTextView.text.toString().substringAfter("\n")
+            put("time", string.substringBefore("\n").trim())
+            var note: String = string.substringAfter("PM")
+            if( note != ""){
+                note = note.substringAfter("Note:")
+            }else{
+                note = ""
+            }
+            put("notes", note.trim())
+        })
+        scheduleArray.put(JSONObject().apply {
+            put("day", "Wednesday")
+            val string: String = wednesdayTextView.text.toString().substringAfter("\n")
+            put("time", string.substringBefore("\n").trim())
+            var note: String = string.substringAfter("PM")
+            if( note != ""){
+                note = note.substringAfter("Note:")
+            }else{
+                note = ""
+            }
+            put("notes", note.trim())
+        })
+        scheduleArray.put(JSONObject().apply {
+            put("day", "Thursday")
+            val string: String = thursdayTextView.text.toString().substringAfter("\n")
+            put("time", string.substringBefore("\n").trim())
+            var note: String = string.substringAfter("PM")
+            if( note != ""){
+                note = note.substringAfter("Note:")
+            }else{
+                note = ""
+            }
+            put("notes", note.trim())
+        })
+        scheduleArray.put(JSONObject().apply {
+            put("day", "Friday")
+            val string: String = fridayTextView.text.toString().substringAfter("\n")
+            put("time", string.substringBefore("\n").trim())
+            var note: String = string.substringAfter("PM")
+            if( note != ""){
+                note = note.substringAfter("Note:")
+            }else{
+                note = ""
+            }
+            put("notes", note.trim())
+        })
+
+        val scheduleObject = JSONObject().apply {
+            put("schedule", scheduleArray)
+        }
+
+        val filename = "schedule.json"
+        val file = File(requireContext().filesDir, filename)
+
+        // Check if the file already exists and contains schedule data
+        if (file.exists()) {
+            val inputStream = FileInputStream(file)
+            val inputString = inputStream.bufferedReader().use { it.readText() }
+            inputStream.close()
+
+            // Compare the existing data to the new data
+            val existingObject = JSONObject(inputString)
+            val existingArray = existingObject.getJSONArray("schedule")
+
+            var hasChanges = false
+            for (i in 0 until existingArray.length()) {
+                val existingDay = existingArray.getJSONObject(i)
+                val newDay = scheduleArray.getJSONObject(i)
+
+                if (existingDay.getString("time") != newDay.getString("time")
+                    || existingDay.getString("notes") != newDay.getString("notes")
+                ) {
+                    hasChanges = true
+                    break
+                }
+            }
+
+            // Write the new data to the file if there are changes
+            if (hasChanges) {
+                val outputStream = FileOutputStream(file)
+                outputStream.write(scheduleObject.toString().toByteArray())
+                outputStream.close()
+                Toast.makeText(
+                    requireContext(),
+                    "Schedule updated and saved to file! ${file.path}",
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else {
+                Toast.makeText(
+                    requireContext(),
+                    "Schedule already up-to-date in file!",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        } else {
+            // Write the new data to the file if it doesn't exist
+            val outputStream = FileOutputStream(file)
+            outputStream.write(scheduleObject.toString().toByteArray())
+            outputStream.close()
+            Toast.makeText(
+                requireContext(),
+                "Schedule saved to file! ${file.path}",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // set the schedule text for each day
-        mondayTextView.text = "Monday:\n9:00 AM - 5:00 PM"
-        tuesdayTextView.text = "Tuesday:\n9:00 AM - 5:00 PM"
-        wednesdayTextView.text = "Wednesday:\n9:00 AM - 5:00 PM"
-        thursdayTextView.text = "Thursday:\n9:00 AM - 5:00 PM"
-        fridayTextView.text = "Friday:\n9:00 AM - 5:00 PM"
+
+        try {
+            loadScheduleFromFile()
+        } catch (e: JSONException) {
+            mondayTextView.text = "Monday:\n9:00 AM - 5:00 PM"
+            tuesdayTextView.text = "Tuesday:\n9:00 AM - 5:00 PM"
+            wednesdayTextView.text = "Wednesday:\n9:00 AM - 5:00 PM"
+            thursdayTextView.text = "Thursday:\n9:00 AM - 5:00 PM"
+            fridayTextView.text = "Friday:\n9:00 AM - 5:00 PM"
+        }catch (e:NoSuchFileException){
+            mondayTextView.text = "Monday:\n9:00 AM - 5:00 PM"
+            tuesdayTextView.text = "Tuesday:\n9:00 AM - 5:00 PM"
+            wednesdayTextView.text = "Wednesday:\n9:00 AM - 5:00 PM"
+            thursdayTextView.text = "Thursday:\n9:00 AM - 5:00 PM"
+            fridayTextView.text = "Friday:\n9:00 AM - 5:00 PM"
+        }
+    }
+
+    private fun loadScheduleFromFile() {
+        val filename = "schedule.json"
+        val file = File(requireContext().filesDir, filename)
+        if (file.exists()) {
+            val inputStream = FileInputStream(file)
+            val size = inputStream.available()
+            val buffer = ByteArray(size)
+            inputStream.read(buffer)
+            inputStream.close()
+            val jsonString = String(buffer, Charsets.UTF_8)
+            val json = JSONObject(jsonString)
+            val scheduleArray = json.getJSONArray("schedule")
+            if (scheduleArray.length() == 5) {
+                mondayTextView.text = "${scheduleArray.getJSONObject(0).getString("day")}: \n${scheduleArray.getJSONObject(0).getString("time")} " +
+                        "${scheduleArray.getJSONObject(0).getString("notes")}"
+                tuesdayTextView.text = "${scheduleArray.getJSONObject(1).getString("day")}: \n${scheduleArray.getJSONObject(1).getString("time")} " +
+                        "${scheduleArray.getJSONObject(1).getString("notes")}"
+                wednesdayTextView.text = "${scheduleArray.getJSONObject(2).getString("day")}: \n${scheduleArray.getJSONObject(2).getString("time")} " +
+                        "${scheduleArray.getJSONObject(2).getString("notes")}"
+                thursdayTextView.text = "${scheduleArray.getJSONObject(3).getString("day")}: \n${scheduleArray.getJSONObject(3).getString("time")} " +
+                        "${scheduleArray.getJSONObject(3).getString("notes")}"
+                fridayTextView.text = "${scheduleArray.getJSONObject(4).getString("day")}: \n${scheduleArray.getJSONObject(4).getString("time")} " +
+                        "${scheduleArray.getJSONObject(4).getString("notes")}"
+                Toast.makeText(requireContext(), "Schedule loaded from file!", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(requireContext(), "Error: Invalid file format!", Toast.LENGTH_SHORT).show()
+            }
+        } else {
+            Toast.makeText(requireContext(), "Error: File not found!", Toast.LENGTH_SHORT).show()
+            throw NoSuchFileException(file)
+        }
     }
 
     private fun updateScheduleText(text: String) {
         scheduleInput.setText(text)
     }
 }
-
