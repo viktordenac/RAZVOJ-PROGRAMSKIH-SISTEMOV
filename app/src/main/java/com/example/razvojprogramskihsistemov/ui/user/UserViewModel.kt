543c6bf6d3dab1327f2cf58264bdc9e02c9b4dba
DanielaButkovska <daniela.butkovska@student.um.si>
2023-05-08 00:41:23 +0200

    Added test code

--- app/src/main/java/com/example/razvojprogramskihsistemov/ui/user/UserViewModel.kt
+++ app/src/main/java/com/example/razvojprogramskihsistemov/ui/user/UserViewModel.kt
@@ -1,4 +1,12 @@
 package com.example.razvojprogramskihsistemov.ui.user
 
-class UserViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class UserViewModel : ViewModel() {
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is user Fragment"
+    }
+    val text: LiveData<String> = _text
 }
