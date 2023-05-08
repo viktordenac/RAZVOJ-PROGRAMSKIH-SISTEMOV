b859cca8288937dd3e9b316bfe8e00ea96f32d11
DanielaButkovska <daniela.butkovska@student.um.si>
2023-05-08 05:40:10 +0200

    Small changes

--- app/src/main/java/com/example/razvojprogramskihsistemov/MainActivity.kt
+++ app/src/main/java/com/example/razvojprogramskihsistemov/MainActivity.kt
@@ -1,18 +1,13 @@
 package com.example.razvojprogramskihsistemov
 
-import CalendarFragment
+import com.example.razvojprogramskihsistemov.ui.calendar.CalendarFragment
 import android.os.Bundle
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
 import com.example.razvojprogramskihsistemov.databinding.ActivityMainBinding
 import com.example.razvojprogramskihsistemov.ui.dashboard.DashboardFragment
 import com.example.razvojprogramskihsistemov.ui.home.HomeFragment
@@ -30,7 +25,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         drawerLayout = findViewById(R.id.drawerLayout)
         val navView : NavigationView = findViewById(R.id.nav_view)
 
@@ -68,13 +62,9 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-
         if(toggle.onOptionsItemSelected(item)){
-
             return true
-
         }
-
         return super.onOptionsItemSelected(item)
     }
 }
